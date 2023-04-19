03a5a6c9f513de6d3753152838fea8c2cfd8b085
Özge Ahras <ozgeahras@gmail.com>
2023-04-19 13:41:25 +0300

    update app.scc

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,7 +13,7 @@ function About() {
             <p>
               My name is Özge and I'm currently living in Istanbul. I work as a
               frontend developer and I'm passionate about building engaging user
-              interfaces. Currently, I'm focusing on learning ReactJS and
+              interfaces. Currently, I'm focusing on building ReactJS and
               improving my skills with this popular JavaScript library. At work,
               I also use AngularJS and Java to develop web applications. I love
               learning and staying up-to-date with the latest technologies and
