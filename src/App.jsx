d10b7c53cb32edfd232685fa4771fe9d52d6be64
Özge Ahras <ozgeahras@gmail.com>
2023-05-04 12:42:34 +0300

    updated import statament

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
 import ReactProjects from "./components/ReactProjects/ReactProjects";
-import JavaScriptProjects from "./components/JavaScriptProjects/JavaScriptProjects";
+import JavaScriptProjects from "./components/JavascriptProjects/JavaScriptProjects";
 import About from "./components/About/About";
 import Footer from "./components/Footer/Footer";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
